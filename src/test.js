8be538bf815bab542873d87cd6c370fa2ae87b1c
Jack Lao <157247502+Junwei-Lao@users.noreply.github.com>
2025-02-03 23:57:36 -0600

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,5 +1,5 @@
-fetch('http://3.82.113.137:6500/receiving_element', {
+fetch('http://127.0.0.1:6500/receiving_element', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ mainText: "What reading events we have there?" })
+    body: JSON.stringify({ mainText: "Please list all the events in categories" })
 }).then(res => {return res.json()}).then(file => {console.log(file)})
